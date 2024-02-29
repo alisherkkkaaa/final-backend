b95642cb20e69323527fc8efd74d78a12f7116dd
alisherkkkaaa <a.kabduldinov@bk.ru>
2024-02-29 18:20:06 +0600

    updated host

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: "final_backend_socail_media_user",
     password: "yFm24IXHmf01tL3pIl2vTD36rZSjamO2",
-    host: "cng6ek021fec73fauli0-a.oregon-postgres.render.com",
+    host: "dpg-cng6ek021fec73fauli0-a.oregon-postgres.render.com",
     port: 5432,
     database: "final_backend_socail_media"
 });
