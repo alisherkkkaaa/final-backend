aff48ecf7367083c637ff13604e6a16e7a0483e3
alisherkkkaaa <a.kabduldinov@bk.ru>
2024-02-29 17:19:21 +0600

    db connect

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: "postgres",
-    password: "Alish2004",
-    host: "localhost",
+    user: "final_backend_socail_media_user",
+    password: "yFm24IXHmf01tL3pIl2vTD36rZSjamO2",
+    host: "dpg-cng6ek021fec73fauli0-a",
     port: 5432,
-    database: "assignment2"
+    database: "final_backend_socail_media"
 });
 
 module.exports = pool;
