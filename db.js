3671ebfbc05e8f82f2ac2bc680a29f0a6f129ff9
alisherkkkaaa <a.kabduldinov@bk.ru>
2024-02-29 18:40:07 +0600

    ss

--- db.js
+++ db.js
@@ -4,7 +4,10 @@ const pool = new Pool({
     password: "yFm24IXHmf01tL3pIl2vTD36rZSjamO2",
     host: "cng6ek021fec73fauli0-a.oregon-postgres.render.com",
     port: 5432,
-    database: "final_backend_socail_media"
+    database: "final_backend_socail_media",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
